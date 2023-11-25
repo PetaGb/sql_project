3aec875c473e04fa71a8aa28472261454ee5d46a
PetaGb <peter.gelec@gmail.com>
2023-11-25 17:01:14 +0100

    test

--- project_PetaG.sql
+++ project_PetaG.sql
@@ -1,10 +1,8 @@
+WITH large_gdp_area AS (
+    SELECT *
+    FROM economies
+    WHERE GDP > 70000000000
+)
 SELECT
-    cp.category_code, cpc.name,
-    cp.region_code, CONCAT(YEAR(cp.date_from), '/', MONTH(cp.date_from)) AS year_and_month,
-    REPLACE(CONCAT(cp.value, ' Kč / ', cpc.price_value, ' ', cpc.price_unit), '.', ',') AS price,
-    RANK() OVER (PARTITION BY cp.category_code ORDER BY cp.value DESC) AS value_rank
-FROM czechia_price cp
-JOIN czechia_price_category cpc
-    ON cp.category_code = cpc.code
-ORDER BY value_rank, cp.value DESC;
-
+    ROUND( AVG(taxes), 2) AS taxes_average
+FROM large_gdp_area;
