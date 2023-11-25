93e5a7be10bb958466310c84e0499502115d5bc3
PetaGb <peter.gelec@gmail.com>
2023-11-25 16:11:35 +0100

    testing push

---
+++ Test.sql
@@ -0,0 +1,9 @@
+SELECT e.country, e.year, e2.year + 1 as year_prev, 
+    round( ( e.GDP - e2.GDP ) / e2.GDP * 100, 2 ) as GDP_growth,
+    round( ( e.population - e2.population ) / e2.population * 100, 2) as pop_growth_percent
+FROM economies e 
+JOIN economies e2 
+    ON e.country = e2.country 
+    AND e.year = e2.year + 1
+    AND e.YEAR < 2020
+ORDER BY GDP_growth DESC 
