0843979b277d738028043194551ed3ebfa210fb1
PetaGb <peter.gelec@gmail.com>
2023-11-30 14:07:28 +0100

    finishing otazka_1

---
+++ otazka_2.sql
@@ -0,0 +1,13 @@
+-- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období
+-- v dostupných datech cen a mezd?
+
+SELECT 
+	food_name,
+	year_sum,
+	price,
+	round(sum(average_salary)/COUNT(average_salary), 0) AS salary,
+	round(sum(average_salary)/COUNT(average_salary)/price, 0) AS food_per_salary
+FROM t_peter_gbelec_project_SQL_primary_final tpgpspf 
+WHERE year_sum IN (2006, 2018)
+	AND food_name IN ('Chléb konzumní kmínový', 'Mléko polotučné pasterované')
+GROUP BY food_name, year_sum 
