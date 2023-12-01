008a26ddd31c5ad53c6311d641d6a26c99011da0
PetaGb <peter.gelec@gmail.com>
2023-12-01 10:56:15 +0100

    finishing otazka_4

--- otazka_4.sql
+++ otazka_4.sql
@@ -1,24 +1,36 @@
 -- Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
 
-SELECT 
-	current_year,
-	prev_year,
-	CONCAT(current_year, ' - ', current_year - 1) AS period,
-	current_salary,
-	prev_salary,
-	current_food_price,
-	prev_food_price
-FROM
-	(SELECT
-		t1.year_sum AS  current_year,
-		t2.year_sum  AS prev_year,
-		round(sum(t1.average_salary)/COUNT(t1.average_salary), 0) AS current_salary,
-		round(sum(t2.average_salary)/COUNT(t2.average_salary), 0) AS prev_salary,
-		round(sum(t1.price)/COUNT(t1.price), 0) AS current_food_price,
-		round(sum(t2.price)/COUNT(t2.price), 0) AS prev_food_price
-	FROM t_peter_gbelec_project_SQL_primary_final t1
-	JOIN t_peter_gbelec_project_SQL_primary_final t2
-		ON t1.year_sum = t2.year_sum + 1
-	GROUP BY t1.year_sum) AS abc
+WITH salary_price_comparison AS (
+	SELECT
+		CONCAT(current_year, ' - ', prev_year) AS period,
+		round(((current_salary - prev_salary)/prev_salary) * 100, 2) AS salary_growth,
+		round(((current_food_price - prev_food_price)/prev_food_price) * 100, 2) AS food_price_growth,
+		current_salary,
+		prev_salary,
+		current_food_price,
+		prev_food_price
+	FROM
+		(SELECT
+			t1.year_sum AS  current_year,
+			t2.year_sum  AS prev_year,
+			round(sum(t1.average_salary)/COUNT(t1.average_salary), 0) AS current_salary,
+			round(sum(t2.average_salary)/COUNT(t2.average_salary), 0) AS prev_salary,
+			round(sum(t1.price)/COUNT(t1.price), 0) AS current_food_price,
+			round(sum(t2.price)/COUNT(t2.price), 0) AS prev_food_price
+		FROM t_peter_gbelec_project_SQL_primary_final t1
+		JOIN t_peter_gbelec_project_SQL_primary_final t2
+			ON t1.year_sum = t2.year_sum + 1
+		GROUP BY t1.year_sum) AS abc)
+SELECT
+	period, 
+	food_price_growth - salary_growth AS real_price_growth,
+	salary_growth,
+	food_price_growth
+FROM salary_price_comparison
+ORDER BY real_price_growth DESC;
+
+	
+
+
 
 	
