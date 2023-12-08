19179fade2d0dcc9015e90f9bd388f3d4661e149
PetaGb <peter.gelec@gmail.com>
2023-12-08 08:48:21 +0100

    add secondary table

---
+++ t_peter_gbelec_project_SQL_secondary_final.sql.sql
@@ -0,0 +1,13 @@
+SELECT 
+    e.country,
+    e.`year`,
+    round(((e.GDP - e2.GDP)/ e2.GDP) * 100, 2) AS gdp_growth
+FROM economies e 
+JOIN economies e2 
+	ON e.country = e2.country
+    and e.`year` = e2.`year` + 1
+WHERE e.country LIKE 'Czech%'
+AND e.year  BETWEEN 2005 AND 2017
+ORDER BY e.year DESC;
+
+
