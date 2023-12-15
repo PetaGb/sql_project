43b632c90a22521d66ea24b85ea1678e0967b916
PetaGb <peter.gelec@gmail.com>
2023-12-15 08:22:58 +0100

    finishing otazka_2

--- otazka_2.sql
+++ otazka_2.sql
@@ -5,9 +5,15 @@ SELECT
 	food_name,
 	year_sum,
 	price,
-	round(sum(average_salary)/COUNT(average_salary), 0) AS salary,
-	round(sum(average_salary)/COUNT(average_salary)/price, 0) AS food_per_salary
+	round(avg(average_salary), 0) AS salary,
+	round(avg(average_salary)/price, 0) AS food_per_salary
 FROM t_peter_gbelec_project_SQL_primary_final tpgpspf 
 WHERE year_sum IN (2006, 2018)
 	AND food_name IN ('Chléb konzumní kmínový', 'Mléko polotučné pasterované')
 GROUP BY food_name, year_sum;
+
+
+
+
+
+
