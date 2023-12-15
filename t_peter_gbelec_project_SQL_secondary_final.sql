387953f252e7db890feb44650255f126bec2d3a3
PetaGb <peter.gelec@gmail.com>
2023-12-15 08:37:30 +0100

    finishing secondary table

--- t_peter_gbelec_project_SQL_secondary_final.sql
+++ t_peter_gbelec_project_SQL_secondary_final.sql
@@ -8,5 +8,5 @@ JOIN economies e2
 	ON e.country = e2.country
     and e.`year` = e2.`year` + 1
 WHERE e.country LIKE 'Czech%'
-AND e.year  BETWEEN 2005 AND 2017
+AND e.year BETWEEN 2005 AND 2017
 ORDER BY e.year DESC);
