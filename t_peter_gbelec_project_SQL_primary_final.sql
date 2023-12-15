11f3e5b5b5cdd7f4ae4a5f7589ca2ae3417a27e7
PetaGb <peter.gelec@gmail.com>
2023-12-15 08:00:09 +0100

    finishing primary table

--- t_peter_gbelec_project_SQL_primary_final.sql
+++ t_peter_gbelec_project_SQL_primary_final.sql
@@ -1,33 +1,27 @@
 CREATE OR REPLACE TABLE t_peter_gbelec_project_SQL_primary_final(
 WITH czechia_payroll_by_year  AS(
 SELECT cpib.name,
-		cp.value, 
-		cp.payroll_year
+	   round(avg(cp.value), 0) AS salary,
+	   cp.payroll_year
 FROM czechia_payroll cp JOIN czechia_payroll_industry_branch cpib 
 	ON cp.industry_branch_code = cpib.code 
 WHERE value_type_code = 5958
-	AND payroll_quarter = 4
 	AND calculation_code = 100
 	AND industry_branch_code IS NOT NULL 
-ORDER BY cp.payroll_year, cp.value),
+GROUP BY cpib.name, cp.payroll_year),
 czechia_price_by_year AS (
 SELECT YEAR(date_to) AS YEAR,
 	   name, 
-	   round(avg(value),2 ) AS price 
+	   round(avg(value), 2) AS price
 FROM czechia_price cp 
 	JOIN  czechia_price_category cpc ON cp.category_code = cpc.code 
 GROUP BY name, YEAR(date_to))
 SELECT a.payroll_year AS year_sum,
 	   a.name AS industry_name,
-	   a.value AS average_salary,
+	   a.salary AS average_salary,
 	   b.name AS food_name, 
 	   b.price
 FROM  czechia_payroll_by_year a LEFT JOIN czechia_price_by_year b 
 	ON a.payroll_year = b.YEAR
 WHERE b.name IS NOT NULL
 GROUP BY a.payroll_year, industry_name, food_name);
-
-
-
-
-
